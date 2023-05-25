dbd073e9f625b17444b9872a73318e1cda2cb8da
Pradeep <pradeepkumarhe1989@gmail.com>
2023-05-25 22:17:28 +0530

    UserWS baseURL kept in properties file

--- src/main/java/com/pradeep/api/projectws/interservicecommunication/UserWS.java
+++ src/main/java/com/pradeep/api/projectws/interservicecommunication/UserWS.java
@@ -4,6 +4,7 @@ package com.pradeep.api.projectws.interservicecommunication;
 import com.pradeep.api.projectws.dtos.response.UserDto;
 import com.pradeep.api.projectws.util.ResourceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +16,19 @@ import java.util.Set;
 
 @Service
 public class UserWS {
+
+    @Value("${UserWSServiceBaseURL}")
+    private String userWSServiceBaseURL;
+
     @Autowired
     private RestTemplate restTemplate;
 
-    private static final String REQUEST_URL_INFO = ">> TinyURL Service URL : ";
+    private static final String REQUEST_URL_INFO = ">> UserWs URL : ";
     private static final String SLASH = "/";
 
     public List<UserDto> getUsers(Set<Long> userIds) {
-        String finalURL= "http://localhost:8080/api/users/userids";
+        String finalURL= userWSServiceBaseURL+"/userids";
         ResponseEntity<List<UserDto>> response = restTemplate.exchange(finalURL, HttpMethod.POST, ResourceUtil.getNewHttpEntity(userIds), new ParameterizedTypeReference<List<UserDto>>() {});
         return response.getBody();
     }
-
-
 }
